d41a48f0fcb6a1edd10fc3b148b709b09bd3498b
Gamygams1234 <gamyburgos@gmail.com>
2021-04-29 23:44:13 -0700

    Displaying all the results which are on our page

--- view.php
+++ view.php
@@ -16,17 +16,33 @@
     <?php
     include('connect-db.php');
 
-// making a connection to the database and making a query
-    if ( $result = $conn ->query('SELECT * FROM players ORDER BY id')){
+    // making a connection to the database and making a query
+    if ($result = $conn->query('SELECT * FROM players ORDER BY id')) {
+        // checking if the rows are larger than 0
+        if ($result->num_rows > 0) {
 
-        echo "Success";
+
+            echo "<table border='1' cellpadding='10'>";
+            echo "<tr><th>ID</th><th>First Name</th><th>Last Name</th></tr>";
+
+            while($row = $result ->fetch_object()){
+                echo "<tr>";
+                echo "<td>" . $row->id ."</td>";
+                echo "<td>" . $row->firstname ."</td>";
+                echo "<td>" . $row->lastname ."</td>";
+                echo"</tr>";
+            }
+            echo "</table>";
+        } else {
+            echo "No results to display.";
+        }
 
 
         // if it did not work then we would run the error
-    }else{
+    } else {
         echo "Error :" . $conn->error;
     }
-    
+    $conn->close();
 
     ?>
 
